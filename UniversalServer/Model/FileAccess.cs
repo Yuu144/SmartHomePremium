fa409d6eb019fa377f2af58fa746d01814b079b9
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2022-03-24 09:17:33 +0100

    Add files via upload

---
+++ UniversalServer/Model/FileAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalServer.Model
+{
+    public delegate void SettingsSavedEventHandler(string msg);
+
+
+    public class FileAccess
+    {
+        public event SettingsSavedEventHandler DataSaved;
+
+        public void SaveSettings(string setting)
+        {
+            //Filestream öffnen
+            //reinschreiben
+            //zu machen
+            //usw.......
+
+            //Wenn das Schreiben geklappt hat, Meldung zurückgeben.
+            DataSaved("Die Daten wurden erfolgreich gespeichert.");
+        }
+    }
+}
